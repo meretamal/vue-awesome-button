b6959495ceecd5dec8c0867a0db52706333a4761
meretamal <meretamal@uc.cl>
2023-02-25 23:32:41 -0300

    feat(aws-btn): add awesone button main structure and props

--- src/components/AwesomeButton/AwesomeButton.tsx
+++ src/components/AwesomeButton/AwesomeButton.tsx
@@ -1,8 +1,32 @@
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 
 export const AwesomeButton = defineComponent({
   name: 'AwesomeButton',
-  setup(_props, { slots }) {
-    return () => <button>{ slots.default?.() }</button>
+  props: {
+    rootElement: {
+      type: String,
+      required: false,
+      default: 'aws-btn',
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'primary'
+    }
+  },
+  setup(props, { slots }) {
+    const rootClasses = computed(() => [    
+      props.rootElement,
+      `${props.rootElement}--${props.type}`,
+    ]) 
+    return () => (
+      <button class={rootClasses.value}>
+        <span class={`${props.rootElement}__wrapper`}>
+          <span class={`${props.rootElement}__content`}>
+            { slots.default?.() }
+          </span>
+        </span>
+      </button>
+    )
   }
-})
+});
