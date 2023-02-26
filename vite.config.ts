1be0fd2bcea4950d8ee0bb376316b4be478926b8
meretamal <meretamal@uc.cl>
2023-02-26 17:46:56 -0300

    chore(build): setup library mode and build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-})
+  build: {
+    lib: {
+      entry: [resolve(__dirname, 'src', 'index.ts')],
+      name: 'VueAwesomeButton',
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    sourcemap: true,
+  },
+});
